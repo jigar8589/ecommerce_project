d3f2c269b629d840c73a59ca734e1b7d88667119
Sagar kanzariya <kanzariya581@gmail.com>
2024-07-23 12:16:39 +0530

    doing fixed

--- service/userService.js
+++ service/userService.js
@@ -5,8 +5,10 @@ const express = require('express')
 
 
 
-async function handlepost(req,res){
-    
+async function handlepost(body){
+    const User = new user(body)
+    const result = await User.save()
+    return result;
 }
 
   
