c275ef737a2f82b4ef07936cdc0ba30a1429a65c
jigar8589 <thakkarjigar8589@gmail.com>
2024-07-23 17:55:39 +0530

    hello

--- service/userService.js
+++ service/userService.js
@@ -1,11 +1,23 @@
 const bcrypt = require("bcryptjs");
 const saltRounds = 10;
+const { genrateOTP } = require("../Util/email");
+const user = require("../model/user.model");
 const {genrateOTP} = require("../Util/email");
 const user=require('../model/user.model')
 const {sendmail} = require("../Util/email");
 
 // Define the manageUser function
 async function manageUser(body) {
+  // const { firstName, lastName, email, phoneNo, password } = body;
+
+  // Encrypt the password
+  const encryptedPassword = await bcrypt.hash(body.password, saltRounds);
+
+  // Generate OTP
+  // const otp = genrateOTP();
+  body.password = encryptedPassword;
+  body.otp = genrateOTP();
+  body.role = "user";
   
     const { firstName, lastName, email, phoneNo, password } = body; 
     
@@ -13,25 +25,15 @@ async function manageUser(body) {
         // Encrypt the password
         const encryptedPassword = await bcrypt.hash(password, saltRounds);
 
-        // Create user in the database
-        const createUser = await user.create({
-            firstName,
-            lastName,
-            email,
-            phoneNo,
-            password: encryptedPassword,
-            otp:genrateOTP()
-        });
+  // Create user in the database
+  const createUser = await user.create(body);
 
+  return createUser;
+}
         return createUser;
   
     }
 
-
 module.exports = {
-    manageUser
+  manageUser,
 };
-
-
-
-
