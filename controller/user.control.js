967adbaa3d548276cae0b43e453a81dec68f7b19
Sagar kanzariya <kanzariya581@gmail.com>
2024-07-25 12:07:39 +0530

    confilict resolved

--- controller/user.control.js
+++ controller/user.control.js
@@ -4,63 +4,42 @@ const userService = require("../service/userService");
 const User = require("../model/user.model");
 // require('../Util/email')
 
-const { manageUser,
+const {
+  manageUser,
   getUser,
   getUserById,
   findUserByEmail,
   checkUserPassword,
-  updatePassword ,
-  userIsActiveCheck}
-  = require("../service/userService")
-const Util = require('../Util/email')
+  updatePassword,
+  userIsActiveCheck,
+} = require("../service/userService");
+const Util = require("../Util/email");
 
 async function handleUser(req, res) {
   try {
-    const userCreate = await userService.manageUser(req.body);
-
-    res.status(200).json({ data: userCreate });
-    //   if (!firstName || !lastName || !email || !phoneNo || !password) {
-    //     res.status(400).send('All fields are required.');
-    // }else{
-    //   res.status(201).send('User registered successfully.');
-    // }
-  } catch (error) {}
-
-
-
-async function handleUser(req, res) {
-
-  try {
-    const userCreate = await manageUser(req.body)
+    const userCreate = await manageUser(req.body);
     if (!userCreate) {
-      res.json({ massage: "User not create" })
+      res.json({ massage: "User not create" });
     } else {
-      const sendmailservice = await Util.sendmail(req.body.email, req.body.otp)
-      res.json({ massage: "User create" })
+      const sendmailservice = await Util.sendmail(req.body.email, req.body.otp);
+      res.json({ massage: "User create" });
     }
-
-    res.status(200).json({ data: userCreate })
-
-
-
+    res.status(200).json({ data: userCreate });
   } catch (error) {
     console.log(error);
   }
-
-  async function sendUseremail(req, res) {}
-
-  //   res.status(200).json({data:userCreate})
-  // } catch (error) {
-  //   console.log("error is ",error);
-  // }
 }
 
 async function handleVerification(req, res) {
   try {
     const verifyUser = await userService.verfiyUser(req.query);
     if (verifyUser) {
-      const updateUser=await user.findOneAndUpdate({_id:verifyUser._id},{isActive:true},{new:true})
-      res.status(200).send({ data: updateUser});
+      const updateUser = await user.findOneAndUpdate(
+        { _id: verifyUser._id },
+        { isActive: true },
+        { new: true }
+      );
+      res.status(200).send({ data: updateUser });
     } else {
       res.status(404).send("user not verified");
     }
@@ -70,74 +49,50 @@ async function handleVerification(req, res) {
   }
 }
 
-module.exports = { handleUser, handleVerification };
-
-
-}
-
 async function getAllUser(req, res) {
   try {
-
-    const Userall = await getUser()
-    res.status(200).send(Userall)
-
+    const Userall = await getUser();
+    res.status(200).send(Userall);
   } catch (error) {
-
     console.log(error);
-
-
   }
 }
-
-
 //get user by id router
 
 async function getUserId(req, res) {
   try {
-    const user = await getUserById(req.params.id)
-    res.status(200).send(user)
+    const user = await getUserById(req.params.id);
+    res.status(200).send(user);
   } catch (error) {
     console.log(error);
   }
 }
 
-
-
 async function resetPassword(req, res) {
-
-  const rest = await findUserByEmail(req.body)
+  const rest = await findUserByEmail(req.body);
   if (rest) {
-
-    const active = await userIsActiveCheck(req.body)
+    const active = await userIsActiveCheck(req.body);
     if (active) {
-      const userPasscheck = await checkUserPassword(req.body)
+      const userPasscheck = await checkUserPassword(req.body);
       if (!userPasscheck) {
-        res.json({ massage: "Email and password incorrect" })
+        res.json({ massage: "Email and password incorrect" });
       } else {
-        const newPassword = await updatePassword(req.body)
-        res.status(200).json({ massage: "password update successfuly" })
+        const newPassword = await updatePassword(req.body);
+        res.status(200).json({ massage: "password update successfuly" });
       }
-
+    } else {
+      res.json({ massage: "user is not active" });
     }
-    else {
-      res.json({ massage: "user is not active" })
-    }
-  } 
-  else {
+  } else {
     // const userpasscheck = await checkUser(req.body)
-    res.send("user  not exist")
+    res.send("user  not exist");
   }
-
-
-
 }
-
-
-
-
 
 module.exports = {
   handleUser,
   getAllUser,
-  getUserId, resetPassword
-}
+  getUserId,
+  resetPassword,
+  handleVerification,
+};
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-  const {handleUser,getAllUser,getUserId,resetPassword} = require('../controller/user.control')
-  const {restpassword} = require("../service/userService")
+const {
+  handleUser,
+  getAllUser,
+  getUserId,
+  resetPassword,
+  handleVerification,
+} = require("../controller/user.control");
 
-  router.get("/",getAllUser)
-  router.get("/:id",getUserId)
-  router.post("/signup",handleUser)
-  router.post("/restpassword",resetPassword)
+router.get("/", getAllUser);
+router.get("/:id", getUserId);
+router.post("/signup", handleUser);
+router.post("/restpassword", resetPassword);
+router.post("/verify", handleVerification);
 
 module.exports = router;
