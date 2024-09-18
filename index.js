4cf075ef969c559d489cad6655a4b106ccab0a62
Sagar kanzariya <kanzariya581@gmail.com>
2024-09-18 18:17:11 +0530

    formatted code

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const router = require("./router/userRouter");
 const productrouter = require("./router/productRouter");
-const addressrouter = require("./router/addressRouter");
-const cartrouter = require("./router/cartRouter");
+const addressRouter = require("./router/addressRouter");
+const cartRouter = require("./router/cartRouter");
 const app = express();
 require("./config");
 app.use(express.json());
@@ -12,8 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users/", router);
 app.use("/api/product", productrouter);
-app.use("/api/address", addressrouter);
-app.use("/api/cart", cartrouter);
+app.use("/api/adress", addressRouter);
+app.use("/api/cart", cartRouter);
 
 app.listen(port, () => {
   console.log(`server run on PORT ${port}`);
