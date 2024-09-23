b99a478b08c507668dc6d3a62cbb4ea63104f9c8
jigar8589 <thakkarjigar8589@gmail.com>
2024-09-23 15:52:35 +0530

    change authentication file

--- middlewere/auth.js
+++ middlewere/auth.js
@@ -38,9 +38,6 @@ const verifyTokenPromise = (token, key, options) => {
 const authentications =  async (req, res, next) => {
   try {
   const  authorization  = req.headers.authorization;
-  if (!authorization) {
-    return res.status(401).json({ error: "Authorization header is not provided" });
-  }
 
   const token = authorization.split(" ")[1]
   const key = process.env.JWT_SECRECT;
